90197de7f501c6953b3dfca6dd6ef1e21beef792
Bibaswan Chakma <98103061+bibaswanchakma02@users.noreply.github.com>
2022-01-20 20:24:39 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    string Name;
+    cout << "enter your name" << endl;
+    cin >> Name;
+    cout << "hi " << Name << " you are dumb xdddd" << endl;
+    
+        
+    
+    return 0;
+}
